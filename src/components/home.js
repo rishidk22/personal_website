0a1fd843fac5bbce42a6bbf7cd40d6ee1e1e1a70
rishidk22 <rishidk22@gmail.com>
2018-08-06 22:34:15 -0500

    added homepage content and set up typist

--- src/components/home.js
+++ src/components/home.js
@@ -1,16 +1,44 @@
 import React, {Component} from 'react';
 import '../App.css';
 
+import Typist from 'react-typist'
+import 'react-typist/dist/Typist.css'
+
+const cursor  = {
+    show: true,
+    blink: true,
+    element: '|',
+    hideWhenDone: true,
+    hideWhenDoneDelay: 500,
+}
+
 class Home extends Component {
 
-    constructor(){
+    constructor() {
         super();
+        this.state = {renderSecond: false}
     }
 
+    setNext = () =>
+        this.setState({renderSecond: true})
+
+
     render() {
-        return (<div>
-            hello
-        </div>);
+        return (
+            <div>
+                <Typist className = 'home-header' startDelay = {500} cursor = {cursor} onTypingDone={this.setNext}>
+                    rishi kalluri
+                </Typist>
+                <div>
+                    {this.state.renderSecond ? (
+                        <Typist className = 'home-content' startDelay = {1500} blink = {true} cursor = {cursor}>
+                            B.E. Computer Engineering 2020, UIUC
+                        </Typist>
+                    ) : null
+                    }
+                </div>
+            </div>
+        );
     }
 }
 
