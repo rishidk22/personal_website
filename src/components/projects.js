a6a22f61c939269e6107871bd3ebdd545627a3e7
rishi.kalluri <rishi.kalluri@physiq.com>
2018-08-08 16:36:11 -0500

    added modal structure for projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -18,26 +18,26 @@ class Projects extends Component {
     render() {
         return (
             <div>
-                <Typist className='home-content'>Click on a project to learn more about it!</Typist>
+                <Typist className='home-content' startDelay={500}>Click on a project to learn more about it!</Typist>
                 <br/>
                 <Card.Group itemsPerRow={2}>
                     <Card raised={true} color='white'>
                         <Card.Content>
-                            <Card.Header>SubStop Chrome Extension & REST API<img className='github-project' src="https://i.pinimg.com/236x/54/7a/96/547a96e289c3c8f10570d170867dbe8a--startup-resume.jpg"/></Card.Header>
+                            <Card.Header>SubStop Chrome Extension & REST API</Card.Header>
                             <SubspotSnippet/>
                             <Card.Description>Chrome Extension with API to detect subscription payments</Card.Description>
                         </Card.Content>
                     </Card>
                     <Card raised={true}>
                         <Card.Content>
-                            <Card.Header>SFPD Dispatch Anlaysis<img className='github-project' src="https://i.pinimg.com/236x/54/7a/96/547a96e289c3c8f10570d170867dbe8a--startup-resume.jpg"/></Card.Header>
+                            <Card.Header>SFPD Dispatch Anlaysis</Card.Header>
                             <SfpdSnippet/>
                             <Card.Description>Analysis and visualization of SFPD dispatch information</Card.Description>
                         </Card.Content>
                     </Card>
                     <Card raised={true}>
                         <Card.Content>
-                            <Card.Header>RunSafe Android Application<img className='github-project' src="https://i.pinimg.com/236x/54/7a/96/547a96e289c3c8f10570d170867dbe8a--startup-resume.jpg"/></Card.Header>
+                            <Card.Header>RunSafe Android Application</Card.Header>
                             <RunsafeSnippet/>
                             <Card.Description>Android application to emergency signal to 911 and contacts</Card.Description>
                         </Card.Content>
